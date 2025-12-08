3fa3e623a4937ded59a71107fb151451645a7b64
Felipe Brito <128190230+felipebrito02@users.noreply.github.com>
2025-12-08 00:53:29 +0000

    att vite and sale

--- client/src/components/SalePopUp.tsx
+++ client/src/components/SalePopUp.tsx
@@ -96,7 +96,7 @@ export default function SocialProofPopup() {
 
     setTimeout(() => {
       setVisible(false);
-    }, 5000); // popup fica visível 4s
+    }, 5000); // popup fica visível 5s
   };
 
   // Aparece imediatamente ao carregar e depois de tempos em tempos
